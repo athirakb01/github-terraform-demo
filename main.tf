1e950e230ab0df03019c3640431eaa65c7830c02
athira <athirakanababu@gmail.com>
2026-08-13 11:40:20 +0000

    adding main

---
+++ main.tf
@@ -0,0 +1,38 @@
+
+# enabling API
+resource "google_project_service" "compute" {
+service = "compute.googleapis.com"
+disable_on_destroy = false
+}
+
+#create vpc
+resource "google_compute_network" "vpc" {
+    name = "my-vpc"
+    auto_create_subnetworks = false
+    depends_on = [google_project_srvice.compute]
+}
+
+#create subnets 
+resource "google_compute_subnetwork" "subnet" {
+  name = "my-subnet"
+  region = "europe-west1"
+  ip_cidr_range = "10.20.1.0/24"
+  network = google_compute_network.vpc.id
+}
+
+# create vm
+resource "google_compute_instance" "vm" {
+    name = "my-vm"
+    machine_type = "e2-medium"
+    zone = "europe-west1-b"
+    boot_disk {
+        initialize_params {
+            image = "debian-11-bullseye-v20240415"
+            size = 20
+        }
+    }
+    network_interface {
+        network = google_compute_network.vpc.id
+        subnetwork = google_compute_network.subnet.id
+    }
+}
