86e9712147f0a63c65f3ea668361e3adb12384bf
athirakb01 <athirakanababu@gmail.com>
2026-08-30 22:29:20 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -41,7 +41,7 @@ resource "google_compute_instance" "vm" {
 resource "google_compute_instance" "vm2" {
     name = "my-vm2"
     machine_type = "e2-micro"
-    zone = "europe-west1-c"
+    zone = "europe-west1-b"
     boot_disk {
         initialize_params {
             image = "debian-11-bullseye-v20240415"
